7945304035bb1159fa30dbdcf26ce62df04edfc2
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-07-20 23:53:54 +0300

    ApiError: use @FieldDefaults annotation

--- core/main-service/src/main/java/ru/practicum/error/ApiError.java
+++ core/main-service/src/main/java/ru/practicum/error/ApiError.java
@@ -1,9 +1,11 @@
 package ru.practicum.error;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,13 +13,14 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ApiError {
-    private List<String> errors;
-    private String message;
-    private String reason;
-    private String status;
+    List<String> errors;
+    String message;
+    String reason;
+    String status;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime timestamp = LocalDateTime.now();
+    LocalDateTime timestamp = LocalDateTime.now();
 
     public ApiError(String status, String reason, String message, List<String> errors) {
         this.status = status;
