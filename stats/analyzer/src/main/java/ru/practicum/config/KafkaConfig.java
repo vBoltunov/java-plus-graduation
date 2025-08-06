26f6120ef3f9ad49e54b1d44c22041004f82fe02
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-08-06 10:25:40 +0300

    Add @FieldDefaults annotation where necessary

--- stats/analyzer/src/main/java/ru/practicum/config/KafkaConfig.java
+++ stats/analyzer/src/main/java/ru/practicum/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package ru.practicum.config;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +13,10 @@ import java.util.Properties;
 @Getter
 @Setter
 @Component
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @ConfigurationProperties("analyzer.kafka")
 public class KafkaConfig {
-    private EnumMap<KafkaTopic, String> topics = new EnumMap<>(KafkaTopic.class);
-    private Properties actionConsumerProps;
-    private Properties similarityConsumerProps;
+    EnumMap<KafkaTopic, String> topics = new EnumMap<>(KafkaTopic.class);
+    Properties actionConsumerProps;
+    Properties similarityConsumerProps;
 }
